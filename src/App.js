f82be7cb67bff6f1b44a42928738cdb991ca9356
DARK-CODE1 <hashemy606@gmail.com>
2024-10-04 19:56:04 +0330

    complete sevice and banner section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import Category from './components/Category/Category'
-import Category2 from './components/Category/Category2'
-import Services from './components/Services/Services'
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+import Category from './components/Category/Category';
+import Category2 from './components/Category/Category2';
+import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
+import Headphone from "./assets/hero/headphone.png";
 
+const BannerData = {
+  discount: '30% OFF',
+  title: 'Fine Smile',
+  date: '10 Jan To 28 Jan',
+  image: Headphone,
+  title2: 'Air Solo Bass',
+  title3: 'Winter Sale',
+  title4: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. In nesciunt beatae eius ad tempora.',
+  bgColor: '#f42c37'
+}
 const App = () => {
   return (
-    <div>
+    <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
       <Navbar />
       <Hero />
-      <Category/>
-      <Category2/>
-      <Services/>
+      <Category />
+      <Category2 />
+      <Services />
+      <Banner data={BannerData} />
     </div>
   )
 }
